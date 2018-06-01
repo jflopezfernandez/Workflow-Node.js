1d64192f866987bacc401809b2f90abafdb39c43
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2018-05-31 23:25:45 -0400

    Making progress on the workflow definition

---
+++ clean.bat
@@ -0,0 +1,2 @@
+
+npm run-script lint && mocha test
---
+++ test/mocha.test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const assert = require("assert");
+
+describe("TestSetupCOnfig", () => {
+	it("should complete this test", () => {
+		assert.ok(true);
+	});
+});
